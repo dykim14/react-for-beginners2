2c8b17a72c691e7c0f4e9627949833c9a8d89d67
dykim14 <dykim14@gmail.com>
2022-03-10 15:58:34 +0900

    Movie app and React router

--- src/App.js
+++ src/App.js
@@ -1,52 +1,16 @@
-import { useEffect, useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Detail from "./routes/Detail";
+import Home from "./routes/Home";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [coinList, setCoinList] = useState([]);
-  const [usd, setUsd] = useState(0);
-  const [perUsd, setPerUsd] = useState(0);
-
-  const onCoinChange = (event) =>
-    setPerUsd(coinList[event.target.selectedIndex].quotes.USD.price);
-  const onInputChange = (event) => setUsd(event.target.value);
-
-  useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-      .then((response) => response.json())
-      .then((json) => {
-        setCoinList(json);
-        setLoading(false);
-        if (json.length > 0) {
-          setPerUsd(json[0].quotes.USD.price);
-        }
-      });
-  }, []);
   return (
-    <div>
-      <h1>The Coins! {loading ? "" : `(${coinList.length})`}</h1>
-      {loading ? (
-        <strong>Loading...</strong>
-      ) : (
-        <div>
-          <input
-            type="number"
-            onChange={onInputChange}
-            min={0}
-            defaultValue={0}
-          />
-          {` USD = ${usd * perUsd} `}
-          <select onChange={onCoinChange}>
-            {coinList.map((coin, index) =>
-              index < 100 ? (
-                <option key={coin.id}>
-                  {coin.name} ({coin.symbol})
-                </option>
-              ) : null
-            )}
-          </select>
-        </div>
-      )}
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/hello" element={<h1>Hello</h1>} />
+        <Route path="/movie" element={<Detail />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
